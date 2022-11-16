f7d4539ece760be076199117c2b0eba35bc5b324
4rmut <hltsofting@gmail.com>
2022-11-16 15:55:07 +0300

    vercel bug fix

--- api/models.js
+++ api/models.js
@@ -1,5 +1,7 @@
 import { JsonDB, Config } from 'node-json-db';
+import path from 'path';
+const dataDirectory = path.join(process.cwd(), 'data');
 
-export const CountriesDB = new JsonDB(new Config('../data/Countries', true, false, '/'));
-export const StatesDB = new JsonDB(new Config('../data/States', true, false, '/'));
-export const CitiesDB = new JsonDB(new Config('../data/Cities', true, false, '/'));
+export const CountriesDB = new JsonDB(new Config(`${dataDirectory}/Countries`, true, false, '/'));
+export const StatesDB = new JsonDB(new Config(`${dataDirectory}/States`, true, false, '/'));
+export const CitiesDB = new JsonDB(new Config(`${dataDirectory}/Cities`, true, false, '/'));
